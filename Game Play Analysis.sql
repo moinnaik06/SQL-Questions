3b669a6ec577fa25bb3a6fe388aea48270885bcf
moinnaik06 <52530558+moinnaik06@users.noreply.github.com>
2023-02-03 14:00:30 +0530

    Create Game Play Analysis.sql

---
+++ Game Play Analysis.sql
@@ -0,0 +1,21 @@
+create table activity (
+
+ player_id     int     ,
+ device_id     int     ,
+ event_date    date    ,
+ games_played  int
+ );
+
+ insert into activity values (1,2,TO_DATE('2016-03-01','YYYY-MM-DD'),5 );
+ insert into activity values (1,2,TO_DATE('2016-03-02','YYYY-MM-DD'),6 );
+ insert into activity values (2,3,TO_DATE('2017-06-25','YYYY-MM-DD'),1 );
+ insert into activity values (3,1,TO_DATE('2016-03-02','YYYY-MM-DD'),0 );
+ insert into activity values (3,4,TO_DATE('2018-07-03','YYYY-MM-DD'),5 );
+commit;
+
+ select * from activity;
+ 
+ --game play analysis 1
+ select player_id, min(event_date)
+from activity
+group by player_id;
